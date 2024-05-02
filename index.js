7254913ab885f546a7b1812456aabd1c28118a0d
ferejbo <felix.rej.bottolfsen@gmail.com>
2024-05-02 16:39:25 +0200

    First draft of the action

---
+++ index.js
@@ -0,0 +1,54 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const { Octokit } = require("@octokit/core");
+
+async function checkSpoofing() {
+  const token = core.getInput("GITHUB_TOKEN");
+  const octokit = new Octokit({ auth: token });
+
+  const { owner, repo } = github.context.repo;
+  const sha = github.sha;
+
+  try {
+    const response = await octokit.request(
+      "GET /repos/{owner}/{repo}/commits/{ref}",
+      {
+        owner: owner,
+        repo: repo,
+        ref: sha,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+
+    if (response.status != 200) {
+      core.setFailed(`Action failed with network error: ${response.status}`);
+    }
+
+    console.log(response, owner, repo, sha);
+
+    const data = response.data;
+
+    const commitAuthor = data.author.login;
+    const commitMessage = data.commit.message;
+
+    const pusher = github.triggering_actor;
+
+    if (commitAuthor !== pusher) {
+      const detailedMismatchMessage = `Mismatch detected in commit "${commitMessage}" (${sha}). Author is "${commitAuthor}" while push actor is "${pusher}"😬`;
+      core.setFailed(detailedMismatchMessage);
+      core.setOutput("mismatch", "true");
+    } else {
+      console.log(
+        `No mismatch detected: Commit by '${commitAuthor}' was also pushed by '${pusher}'.`
+      );
+      core.setOutput("mismatch", "false");
+    }
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error}`);
+  }
+}
+
+checkSpoofing();
